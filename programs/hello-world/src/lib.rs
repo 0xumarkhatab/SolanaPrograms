6daa468feb6893843dbd381e915d147d46785ad4
superForgerer <umarkhatabfrl@gmail.com>
2025-03-10 01:07:27 +0000

    added hello world

---
+++ programs/hello-world/src/lib.rs
@@ -0,0 +1,36 @@
+use anchor_lang::prelude::*;
+
+declare_id!("4FwiingdtrzTqErSzxmKnMDLhV3iUj4CiCv4dveWp8ps");
+
+#[program]
+pub mod hello_world {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>,msg:String) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        let data_account: &mut Account<'_, DataAccount>     = &mut ctx.accounts.data_account;
+        data_account.msg=msg;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer:Signer<'info>,
+    #[account{
+            init,
+            payer=signer,
+            space=300,
+        }]
+    pub data_account:Account<'info,DataAccount>,
+    pub system_program:Program<'info,System>
+
+}
+
+#[account]
+pub struct DataAccount{
+    pub msg:String
+}
+
+
---
+++ tests/hello-world.ts
@@ -0,0 +1,66 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { HelloWorld } from "../target/types/hello_world";
+
+
+
+describe("hello-world",async () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.HelloWorld as Program<HelloWorld>;
+  const signer = anchor.web3.Keypair.generate();
+  const data_account = anchor.web3.Keypair.generate();
+
+  // await program.provider.connection.confirmTransaction(await program.provider.connection.requestAirdrop(signer.publicKey,100*anchor.web3.LAMPORTS_PER_SOL),"finalized")
+
+  
+
+  it("Is initialized!", async () => {
+  let x =await program.provider.connection.requestAirdrop(signer.publicKey,100*anchor.web3.LAMPORTS_PER_SOL)
+  console.log("airdrop tx is ",x);
+  let conf_tx = await program.provider.connection.confirmTransaction(x,
+    "finalized"
+  )
+  console.log("conf ",conf_tx);
+  
+
+    // Add your test here.
+    const tx = await program.methods.initialize("Hello World").accounts({
+      signer:signer.publicKey,
+      dataAccount:data_account.publicKey
+    }).signers([signer,data_account]).rpc();
+    console.log("Your transaction signature", tx);
+
+  });
+
+
+  /**
+   * 
+   *     try{
+    const airdropTx = await program.provider.connection.requestAirdrop(
+      signer.publicKey,
+      100 * anchor.web3.LAMPORTS_PER_SOL
+    );
+    console.log("airdrop tx", airdropTx);
+
+    const airdropConf = await program.provider.connection.confirmTransaction(
+      airdropTx,
+      "finalized"
+    );
+    // Add your test here.
+    const txSignature = await program.methods
+      .initialize("Hello World")
+      .accounts({
+        signer: signer.publicKey,
+        dataAccount: data_account.publicKey,
+      })
+      .signers([signer, data_account])
+      .rpc();
+    console.log("Your transaction signature", txSignature);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+   */
+});
